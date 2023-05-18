4bff1447429d166aa27e17bab0ff6a17d1d35025
lassepe <lasse.peters@mailbox.org>
2023-05-19 00:53:06 +0200

    Add examples/Demo.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using MCPTrajectoryGameSolver: MCPTrajectoryGameSolver, OpenLoopStrategy
 
 using TrajectoryGamesBase
 using Test: @test, @testset, @test_throws
-using BlockArrays: Block, eachblock, mortar
+using BlockArrays: Block, blocks, mortar
 using LinearAlgebra: norm, norm_sqr
 using TrajectoryGamesExamples: planar_double_integrator
 using StatsBase: mean
@@ -10,97 +10,22 @@ using Zygote: Zygote
 using FiniteDiff: FiniteDiff
 using Random: Random
 
-function shared_collision_avoidance_coupling_constraints(num_players, min_distance)
-    function coupling_constraint(xs, us)
-        mapreduce(vcat, 1:(num_players - 1)) do player_i
-            mapreduce(vcat, (player_i + 1):num_players) do paired_player
-                map(xs) do x
-                    my_norm_sqr(x[Block(player_i)][1:2] - x[Block(paired_player)][1:2]) -
-                    min_distance^2
-                end
-            end
-        end
-    end
-end
+include("../examples/Demo.jl")
 
-function my_norm_sqr(x)
-    x' * x
-end
+# integration test of the demo
+Demo.demo()
 
-function two_player_guidance_game_with_collision_avoidance(;
-    environment,
-    min_distance = 1.0,
-    hard_constraints = true,
-    collision_avoidance_coefficient = 0,
-    dynamics = planar_double_integrator(;
-        state_bounds = (; lb = [-Inf, -Inf, -0.8, -0.8], ub = [Inf, Inf, 0.8, 0.8]),
-        control_bounds = (; lb = [-10, -10], ub = [10, 10]),
-    ),
-)
-    cost = let
-        function target_cost(x, context_state)
-            norm_sqr(x[1:2] - context_state[1:2])
-        end
-        function control_cost(u)
-            norm_sqr(u)
-        end
-        function cost_for_player1(xs, us, context_state)
-            mean_target = mean(map(xs) do x
-                target_cost(x[Block(1)], context_state)
-            end)
-            control = mean(map(us) do u
-                control_cost(u[Block(1)])
-            end)
-            safe_distance_violation = mean(
-                map(xs) do x
-                    max(0, min_distance + 0.2 - norm(x[Block(1)][1:2] - x[Block(2)][1:2]))^3
-                end,
-            )
-            1.0 * mean_target +
-            0.1 * control +
-            collision_avoidance_coefficient * safe_distance_violation
-        end
-        function cost_for_player2(xs, us)
-            mean_target = mean(map(xs) do x
-                target_cost(x[Block(2)], x[Block(1)])
-            end)
-            control = mean(map(us) do u
-                control_cost(u[Block(2)])
-            end)
-            safe_distance_violation = mean(
-                map(xs) do x
-                    max(0, min_distance + 0.2 - norm(x[Block(1)][1:2] - x[Block(2)][1:2]))^3
-                end,
-            )
-            1.0 * mean_target +
-            0.1 * control +
-            collision_avoidance_coefficient * safe_distance_violation
-        end
-        function cost_function(xs, us, context_state)
-            cost1 = cost_for_player1(xs, us, context_state)
-            cost2 = cost_for_player2(xs, us)
-            [cost1, cost2]
-        end
-        TrajectoryGameCost(cost_function, GeneralSumCostStructure())
-    end
-    dynamics = ProductDynamics([dynamics for _ in 1:2])
-    coupling_constraints =
-        hard_constraints ? shared_collision_avoidance_coupling_constraints(2, min_distance) :
-        nothing
-    TrajectoryGame(dynamics, cost, environment, coupling_constraints)
-end
-
-function isfeasible(game::TrajectoryGamesBase.TrajectoryGame, trajectory; tol = 1e-4)
+function isfeasible(game::TrajectoryGamesBase.TrajectoryGame, trajectory; tol=1e-4)
     isfeasible(game.dynamics, trajectory; tol) &&
         isfeasible(game.env, trajectory; tol) &&
         all(game.coupling_constraints(trajectory.xs, trajectory.us) .>= 0 - tol)
 end
 
-function isfeasible(dynamics::TrajectoryGamesBase.AbstractDynamics, trajectory; tol = 1e-4)
+function isfeasible(dynamics::TrajectoryGamesBase.AbstractDynamics, trajectory; tol=1e-4)
     dynamics_steps_consistent = all(
         map(2:length(trajectory.xs)) do t
             residual =
-                trajectory.xs[t] - dynamics(trajectory.xs[t - 1], trajectory.us[t - 1], t - 1)
+                trajectory.xs[t] - dynamics(trajectory.xs[t-1], trajectory.us[t-1], t - 1)
             sum(abs, residual) < tol
         end,
     )
@@ -122,7 +47,7 @@ function isfeasible(dynamics::TrajectoryGamesBase.AbstractDynamics, trajectory; 
     dynamics_steps_consistent && state_bounds_feasible && control_bounds_feasible
 end
 
-function isfeasible(env::TrajectoryGamesBase.PolygonEnvironment, trajectory; tol = 1e-4)
+function isfeasible(env::TrajectoryGamesBase.PolygonEnvironment, trajectory; tol=1e-4)
     trajectory_per_player = MCPTrajectoryGameSolver.unstack_trajectory(trajectory)
 
     map(enumerate(trajectory_per_player)) do (ii, trajectory)
@@ -134,10 +59,9 @@ function isfeasible(env::TrajectoryGamesBase.PolygonEnvironment, trajectory; tol
 end
 
 function main()
-    environment = TrajectoryGamesBase.PolygonEnvironment(4, 4)
-    game = two_player_guidance_game_with_collision_avoidance(; environment)
+    game = Demo.simple_game()
     horizon = 2
-    context = [1, 1]
+    context = [0.0, 1.0, 0.0, 1.0]
     initial_state = mortar([[1.0, 0, 0, 0], [-1.0, 0, 0, 0]])
 
     local solver, solver_wrong_context
@@ -145,9 +69,9 @@ function main()
     @testset "Tests" begin
         @testset "solver setup" begin
             solver =
-                MCPTrajectoryGameSolver.Solver(game, horizon; context_dimension = length(context))
+                MCPTrajectoryGameSolver.Solver(game, horizon; context_dimension=length(context))
             solver_wrong_context =
-                MCPTrajectoryGameSolver.Solver(game, horizon; context_dimension = 4)
+                MCPTrajectoryGameSolver.Solver(game, horizon; context_dimension=6)
         end
 
         @testset "solve" begin
@@ -170,19 +94,19 @@ function input_sanity(; solver, game, initial_state, context)
             solver_wrong_context,
             game,
             initial_state;
-            context,
+            context
         )
         @test_throws ArgumentError TrajectoryGamesBase.solve_trajectory_game!(
             solver,
             game,
             initial_state;
             context,
-            shared_constraint_premultipliers = [1],
+            shared_constraint_premultipliers=[1]
         )
     end
 end
 
-function forward_pass_sanity(; solver, game, initial_state, context, horizon, strategy, tol = 1e-4)
+function forward_pass_sanity(; solver, game, initial_state, context, horizon, strategy, tol=1e-4)
     @testset "forwardpass sanity" begin
         nash_trajectory =
             TrajectoryGamesBase.rollout(game.dynamics, strategy, initial_state, horizon)
@@ -213,8 +137,8 @@ function backward_pass_sanity(;
     solver,
     game,
     initial_state,
-    rng = Random.MersenneTwister(1),
-    θs = [randn(rng, 2) for _ in 1:10],
+    rng=Random.MersenneTwister(1),
+    θs=[randn(rng, 4) for _ in 1:10]
 )
     @testset "backward pass sanity" begin
         function loss(θ)
@@ -223,7 +147,7 @@ function backward_pass_sanity(;
                     solver,
                     game,
                     initial_state;
-                    context = θ,
+                    context=θ
                 )
 
                 sum(strategy.substrategies) do substrategy
@@ -237,7 +161,7 @@ function backward_pass_sanity(;
         for θ in θs
             ∇_zygote = Zygote.gradient(loss, θ) |> only
             ∇_finitediff = FiniteDiff.finite_difference_gradient(loss, θ)
-            @test isapprox(∇_zygote, ∇_finitediff; atol = 1e-4)
+            @test isapprox(∇_zygote, ∇_finitediff; atol=1e-4)
         end
     end
 end
